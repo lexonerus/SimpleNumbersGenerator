7c14b02e5468e1e2b2697f4f6873b8bc1d5f3857
Alexey Krzywicki <lexone01@gmail.com>
2022-07-23 14:52:24 +0300

    Refactoring

--- SoftProductTestTask/Model/Constants.swift
+++ SoftProductTestTask/Model/Constants.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct Constants {
-    static let title = "Генератор чисел"
-    static let primeTitle = "Простые числа"
-    static let fiboTitle = "Числа Фибоначчи"
-    static let limit = 999_999_999_999
+    static let title        = "Генератор чисел"
+    static let primeTitle   = "Простые числа"
+    static let fiboTitle    = "Числа Фибоначчи"
+    static let limit        = 999_999_999_999
 }
